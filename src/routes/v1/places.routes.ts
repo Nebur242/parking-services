f653b3a2189d4b56fe6489f09675e743de4d9ec0
Ruben <rm.nzaou.etudes@gmail.com>
2022-11-20 03:44:16 +0100

    fix: removed auth middleware

--- src/routes/v1/places.routes.ts
+++ src/routes/v1/places.routes.ts
@@ -2,12 +2,11 @@ import express from 'express';
 import validate from '../../middlewares/validate';
 import validators from '../../modules/places/validators';
 import placesController from '../../modules/places/places.controller';
-import { Roles } from '../../config/roles';
 import { auth } from '../../middlewares/auth';
 
 const router = express.Router();
 
-router.get('/', auth(), placesController.findAll);
+router.get('/', validate(validators.filterQuery), placesController.findAll);
 router.post('/', validate(validators.createPlace), placesController.create);
 router.get('/:id', validate(validators.findOne), placesController.findOne);
 router.patch('/:id', validate(validators.updatePlace), placesController.update);
