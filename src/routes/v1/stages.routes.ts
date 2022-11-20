350fc8eab3b67ade657af8f6075efebfb5c7521e
Ruben <rm.nzaou.etudes@gmail.com>
2022-11-21 00:32:08 +0100

    feat: protected stages cud routes

--- src/routes/v1/stages.routes.ts
+++ src/routes/v1/stages.routes.ts
@@ -2,13 +2,31 @@ import express from 'express';
 import stagesController from '../../modules/stages/stages.controller';
 import validators from '../../modules/stages/validators';
 import validate from '../../middlewares/validate';
+import { auth } from '../../middlewares/auth';
+import { Roles } from '../../config/roles';
 
 const router = express.Router();
 
 router.get('/', stagesController.findAll);
-router.post('/', validate(validators.createStage), stagesController.create);
 router.get('/:id', validate(validators.findOne), stagesController.findOne);
-router.patch('/:id', validate(validators.updateStage), stagesController.update);
-router.delete('/:id', validate(validators.findOne), stagesController.delete);
+
+router.post(
+	'/',
+	auth([Roles.ADMIN]),
+	validate(validators.createStage),
+	stagesController.create
+);
+router.patch(
+	'/:id',
+	auth([Roles.ADMIN]),
+	validate(validators.updateStage),
+	stagesController.update
+);
+router.delete(
+	'/:id',
+	auth([Roles.ADMIN]),
+	validate(validators.findOne),
+	stagesController.delete
+);
 
 export default router;
