c618e90117333d8b0429688b219eddfd9b3df906
Ruben <rm.nzaou.etudes@gmail.com>
2022-11-20 03:58:50 +0100

    feat: added route protection by role

--- src/routes/v1/places.routes.ts
+++ src/routes/v1/places.routes.ts
@@ -3,13 +3,29 @@ import validate from '../../middlewares/validate';
 import validators from '../../modules/places/validators';
 import placesController from '../../modules/places/places.controller';
 import { auth } from '../../middlewares/auth';
+import { Roles } from '../../config/roles';
 
 const router = express.Router();
 
 router.get('/', validate(validators.filterQuery), placesController.findAll);
-router.post('/', validate(validators.createPlace), placesController.create);
+router.post(
+	'/',
+	auth([Roles.ADMIN]),
+	validate(validators.createPlace),
+	placesController.create
+);
 router.get('/:id', validate(validators.findOne), placesController.findOne);
-router.patch('/:id', validate(validators.updatePlace), placesController.update);
-router.delete('/:id', validate(validators.findOne), placesController.delete);
+router.patch(
+	'/:id',
+	auth([Roles.ADMIN]),
+	validate(validators.updatePlace),
+	placesController.update
+);
+router.delete(
+	'/:id',
+	auth([Roles.ADMIN]),
+	validate(validators.findOne),
+	placesController.delete
+);
 
 export default router;
