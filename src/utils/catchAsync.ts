ef0926591d917791c446e258031c6426352893f2
Ruben <rm.nzaou.etudes@gmail.com>
2022-11-20 03:43:33 +0100

    feat: added global response format

--- src/utils/catchAsync.ts
+++ src/utils/catchAsync.ts
@@ -1,8 +1,32 @@
 import { Request, Response, NextFunction } from 'express';
+import httpStatus from 'http-status';
+
+//'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+const getStatusCode = (method: string): number => {
+	switch (method) {
+		case 'GET':
+		case 'PUT':
+		case 'PATCH':
+		case 'DELETE':
+			return httpStatus.OK;
+		case 'POST':
+			return httpStatus.CREATED;
+		default:
+			return httpStatus.OK;
+	}
+};
 
 const catchAsync =
 	(fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-		Promise.resolve(fn(req, res, next)).catch((err) => next(err));
+		Promise.resolve(fn(req, res, next))
+			.then((data) => {
+				const statusCode = getStatusCode(req.method);
+				res.status(httpStatus.OK).json({
+					statusCode,
+					data,
+				});
+			})
+			.catch((err) => next(err));
 	};
 
 export { catchAsync };
