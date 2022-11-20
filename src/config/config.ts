0241786405edf05c1baa1753393f4204fb734258
Ruben <rm.nzaou.etudes@gmail.com>
2022-11-20 01:32:44 +0100

    feat: added config files

--- src/config/config.ts
+++ src/config/config.ts
@@ -13,6 +13,13 @@ interface IConfig {
 		uri: string;
 		options: mongoose.ConnectOptions;
 	};
+	jwt: {
+		secret: string;
+		accessExpirationMinutes: number;
+		refreshExpirationDays: number;
+		resetPasswordExpirationMinutes: number;
+		verifyEmailExpirationMinutes: number;
+	};
 }
 
 const config: IConfig = {
@@ -21,6 +28,19 @@ const config: IConfig = {
 	mongoose: {
 		uri: process.env.MONGODB_URI,
 		options: {},
+	},
+	jwt: {
+		secret: process.env.JWT_SECRET,
+		accessExpirationMinutes: Number(
+			process.env.JWT_ACCESS_EXPIRATION_MINUTES
+		),
+		refreshExpirationDays: Number(process.env.JWT_REFRESH_EXPIRATION_DAYS),
+		resetPasswordExpirationMinutes: Number(
+			process.env.JWT_RESET_PASSWORD_EXPIRATION_MINUTES
+		),
+		verifyEmailExpirationMinutes: Number(
+			process.env.JWT_VERIFY_EMAIL_EXPIRATION_MINUTES
+		),
 	},
 };
 
---
+++ src/config/passport.ts
@@ -0,0 +1,29 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { User } from '../modules/users/models/user.model';
+import config from './config';
+import { TokenTypes } from './tokens';
+
+const jwtOptions = {
+	secretOrKey: config.jwt.secret,
+	jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+};
+
+const jwtVerify = async (payload, done: Function) => {
+	try {
+		console.log(payload);
+		if (payload.type !== TokenTypes.ACCESS) {
+			throw new Error('Invalid token type');
+		}
+		const user = await User.findById(payload.sub);
+		if (!user) {
+			return done(null, false);
+		}
+		done(null, user);
+	} catch (error) {
+		done(error, false);
+	}
+};
+
+const jwtStrategy = new Strategy(jwtOptions, jwtVerify);
+
+export { jwtStrategy };
