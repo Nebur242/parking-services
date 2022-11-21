cab2d95d6538cf9fc1376652ae704997671b8ad6
Ruben <rm.nzaou.etudes@gmail.com>
2022-11-21 22:29:59 +0100

    fix: chain router for same routes endpoints

--- src/routes/v1/stages.routes.ts
+++ src/routes/v1/stages.routes.ts
@@ -7,26 +7,27 @@ import { Roles } from '../../config/roles';
 
 const router = express.Router();
 
-router.get('/', stagesController.findAll);
-router.get('/:id', validate(validators.findOne), stagesController.findOne);
+router
+	.route('/')
+	.get(stagesController.findAll)
+	.post(
+		auth([Roles.ADMIN]),
+		validate(validators.createStage),
+		stagesController.create
+	);
 
-router.post(
-	'/',
-	auth([Roles.ADMIN]),
-	validate(validators.createStage),
-	stagesController.create
-);
-router.patch(
-	'/:id',
-	auth([Roles.ADMIN]),
-	validate(validators.updateStage),
-	stagesController.update
-);
-router.delete(
-	'/:id',
-	auth([Roles.ADMIN]),
-	validate(validators.findOne),
-	stagesController.delete
-);
+router
+	.route('/:id')
+	.get(validate(validators.findOne), stagesController.findOne)
+	.patch(
+		auth([Roles.ADMIN]),
+		validate(validators.updateStage),
+		stagesController.update
+	)
+	.delete(
+		auth([Roles.ADMIN]),
+		validate(validators.findOne),
+		stagesController.delete
+	);
 
 export default router;
